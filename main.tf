5a165e623539943e16626159e99de9502d23e866
Sanjay <scshitole@gmail.com>
2022-05-04 17:35:02 -0700

    added

--- main.tf
+++ main.tf
@@ -27,3 +27,16 @@ resource "bigip_fast_template" "consul-webinar" {
   md5_hash = filemd5("ConsulWebinar.zip")
   depends_on = [data.archive_file.template_zip]
 }
+
+resource "bigip_fast_application" "nginx-webserver" {
+  template        = "ConsulWebinar/ConsulWebinar"
+  fast_json   = <<EOF
+{
+      "tenant": "Consul_SD",
+      "app": "Nginx",
+      "virtualAddress": "10.0.0.200",
+      "virtualPort": 8080
+}
+EOF
+  depends_on = [bigip_fast_template.consul-webinar]
+}
