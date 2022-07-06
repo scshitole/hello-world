ad2d114bf1d774dd7396a20cf950b247fd1a7cf9
Sanjay <scshitole@gmail.com>
2022-07-05 22:01:11 -0700

    created with waf

--- main.tf
+++ main.tf
@@ -17,19 +17,19 @@ terraform {
 
 data "archive_file" "template_zip" {
   type        = "zip"
-  source_file = "FastWAF.yaml"
-  output_path = "FastWAF.zip"
+  source_file = "ConsulWebinar.yaml"
+  output_path = "ConsulWebinar.zip"
 }
 
 resource "bigip_fast_template" "consul-webinar" {
-  name = "FastWAF"
-  source = "FastWAF.zip"
-  md5_hash = filemd5("FastWAF.zip")
+  name = "ConsulWebinar"
+  source = "ConsulWebinar.zip"
+  md5_hash = filemd5("ConsulWebinar.zip")
   depends_on = [data.archive_file.template_zip]
 }
 
  resource "bigip_fast_application" "nginx-webserver" {
-  template        = "FastWAF/FastWAF"
+  template        = "ConsulWebinar/ConsulWebinar"
   fast_json   = <<EOF
 {
       "tenant": "Consul_SD",
@@ -43,7 +43,7 @@ EOF
 }
 
 resource "bigip_fast_application" "nginx-app100" {
-  template        = "FastWAF/FastWAF"
+  template        = "ConsulWebinar/ConsulWebinar"
   fast_json   = <<EOF
 {
       "tenant": "App100",
@@ -56,7 +56,7 @@ EOF
   depends_on = [bigip_fast_template.consul-webinar]
 }
 resource "bigip_fast_application" "bbnginx-webserver" {
-  template        = "FastWAF/FastWAF"
+  template        = "ConsulWebinar/ConsulWebinar"
   fast_json   = <<EOF
 {
       "tenant": "S22Consul_SD",
